50e8acf6fa1ac9877c49575fde9dffe9cdfd5d1b
tom.kajiwara <tomkaji555@gmail.com>
2019-06-22 06:32:01 +0000

    fix application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # SessionsHelperに記載されているメソッドを、自分のインスタンスメソッドとして取り込むことができる。
+    # これをMix-inという。
+    include SessionsHelper
+
+private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @micropost = current_user.microposts.build #form_width 用
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page]) #form_width 用
+    end
   end
 end
